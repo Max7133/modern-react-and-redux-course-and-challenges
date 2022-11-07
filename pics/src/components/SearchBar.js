bf65dd3a26488d522c5ec611393ac7f8b70f617e
Maksim Pasnitsenko <emexpas@outlook.com>
2022-11-07 13:52:15 +0200

    Pics app: added state to the input field on the search bar

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,30 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  // when typing something to the Input, this Callback is going to be invoked (with 1 arg being passed to it automatically)
+  // Naming Rule: ON the name of the element that we are assigning this callback to, IN (input), and then the Event that we are watching for.
+  // onInputChange(e) {
+  //   e.target.value; // contains the Text that the User just added to the Input
+  // }
+  // <input type="text" onChange={this.onInputChange} />
+
+  // Initializing State
+  // State is an Object that has a single Property 'term' (empty String by default)
+  state = { term: "" };
+  // After initializing the State it has a single Property 'term' that starts off as an Empty String
+  // And then every single time a User types inside that Input, the State is going to be updated ('term' Property)
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.term} // the Value Prop is going to overwrite whatever text is already inside of the Input
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
