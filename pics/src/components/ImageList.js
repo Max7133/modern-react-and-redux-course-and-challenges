374ff56f45afb95a42c40c565a10025757ab0ee8
Maksim Pasnitsenko <emexpas@outlook.com>
2022-11-12 14:01:10 +0200

    Pics app: some css added for the images + keys

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,14 +1,19 @@
+import "./ImageList.css";
 import React from "react";
 
 const ImageList = (props) => {
   // will be called with each image from list of images
-  const images = props.images.map((image) => {
+  // destructuring of 'image' so I wont need to write it 3 times in the 'return'
+  const images = props.images.map(({ description, id, urls }) => {
     // returns a Img Tag that has the appropriate source.
-    return <img src={image.urls.regular} />;
+    return (
+      //<img alt={image.description} key={image.id} src={image.urls.regular} />
+      <img alt={description} key={id} src={urls.regular} />
+    ); // adding a Key Property to the root Tag that I'm returning from the map()
   });
 
   // Taking the 'images' Array that gets returned from the Map statement and return it from the ImageList
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
