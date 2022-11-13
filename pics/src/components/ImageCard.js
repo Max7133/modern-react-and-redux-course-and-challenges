1058a5b711806061850077cdb8655a832d29cbcd
Maksim Pasnitsenko <emexpas@outlook.com>
2022-11-13 21:05:32 +0200

    Pics app: created a ImageCard component

---
+++ pics/src/components/ImageCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+// Component that checks the IMG how large it is, and sets the grid-row-end: INDIVIDUALLY
+class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Calling React.createRef() and assign it to the Instance Variable so that I can refer back to later on inside this Class
+    this.imageRef = React.createRef(); // for accessing a single element in the DOM, in this case an IMG
+  }
+
+  // this will be called after the ImageCard and it's image gets rendered
+  componentDidMount() {
+    // Gives a direct reference to the img Tag and then Ill add an Event Listener to it. (like in JS)
+    // Any time that a image successfully loads up or downloads an Image from the Internet and displays it on the screen, it emits this (load) Event
+    // Once it emits this load event, that means now it has successfully downloaded the Image, its being displayed on the screen, and now I can get the height of the Image
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  // calling setSpans here is because the Property that I'm gone set inside it is called the (grid-row-end Property) and it took units of spans.
+  // Arrow function because of the This Keyword
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  };
+
+  render() {
+    // destructuring, so I wont write this.props.image.description, this.props.image.urls.regular
+    const { description, urls } = this.props.image;
+
+    // ref is a JS Object that has a 'current' Property and it references a DOM NODE in this case is a img
+    return (
+      <div>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
+      </div>
+    );
+  }
+}
+
+export default ImageCard;
--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,14 +1,17 @@
 import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
   // will be called with each image from list of images
   // destructuring of 'image' so I wont need to write it 3 times in the 'return'
-  const images = props.images.map(({ description, id, urls }) => {
+  const images = props.images.map((imageProp) => {
     // returns a Img Tag that has the appropriate source.
     return (
       //<img alt={image.description} key={image.id} src={image.urls.regular} />
-      <img alt={description} key={id} src={urls.regular} />
+      // 2nd way
+      //<img alt={description} key={id} src={urls.regular} />
+      <ImageCard key={imageProp.id} image={imageProp} />
     ); // adding a Key Property to the root Tag that I'm returning from the map()
   });
 
