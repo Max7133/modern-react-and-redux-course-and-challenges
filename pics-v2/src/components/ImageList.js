74798ea0ef3b82209160687e89fc3b83d699d563
Maksim Pasnitsenko <emexpas@outlook.com>
2022-12-01 17:56:18 +0200

    pics-v2: app for searching images done

--- pics-v2/src/components/ImageList.js
+++ pics-v2/src/components/ImageList.js
@@ -1,5 +1,11 @@
-function ImageList() {
-  return <div>ImageList</div>;
+import './ImageList.css';
+import ImageShow from './ImageShow';
+
+function ImageList({ img }) {
+  const renderedImages = img.map(image => {
+    return <ImageShow key={image.id} img={image} />;
+  });
+  return <div className="image-list">{renderedImages}</div>;
 }
 
 export default ImageList;
--- pics-v2/src/components/ImageShow.js
+++ pics-v2/src/components/ImageShow.js
@@ -1,5 +1,9 @@
-function ImageShow() {
-  return <div>ImageShow</div>;
+function ImageShow({ img }) {
+  return (
+    <div>
+      <img src={img.urls.small} alt={img.alt_description} />
+    </div>
+  );
 }
 
 export default ImageShow;
