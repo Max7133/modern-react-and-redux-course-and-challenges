c0050609ac4f8391e66a5e66ea52552eb9f94e8d
Maksim Pasnitsenko <emexpas@outlook.com>
2023-05-31 13:02:45 +0300

    counts the number of books that have been added

--- books/src/App.js
+++ books/src/App.js
@@ -5,10 +5,14 @@ function App() {
   const [books, setBooks] = useState([]);
 
   // receives whatever title of a book user has entered
-  const createBook = title => {};
+  const createBook = title => {
+    const updatedBooks = [...books, { id: 123, title }];
+    setBooks(updatedBooks);
+  };
 
   return (
     <div>
+      {books.length}
       <BookCreate onCreate={createBook} />
     </div>
   );
