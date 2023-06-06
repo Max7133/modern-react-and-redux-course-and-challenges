3ce45db1bfa5690d26b463895993ccb2ebd6c2b9
Maksim Pasnitsenko <emexpas@outlook.com>
2023-06-06 14:59:36 +0300

    edit books with form

--- books/src/components/BookEdit.js
+++ books/src/components/BookEdit.js
@@ -1,5 +1,27 @@
-function BookEdit() {
-  return <div>BookEdit</div>;
+import { useState } from 'react';
+
+function BookEdit({ book }) {
+  const [title, setTitle] = useState(book.title); // default Value - empty string
+
+  // This will be called whenever a user changes the text inputs
+  const handleChange = e => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  return (
+    // Whenever a user presses the ENTER key or clicks on the Button to submit the form
+    <form onSubmit={handleSubmit} className="book-edit">
+      <label>Title</label>
+      {/* Assigning the "title" & "handleChange" event handler to the Input element */}
+      <input className="input" value={title} onChange={handleChange} />
+      {/* Whenever a user presses the ENTER key or clicks on the Button to submit the form */}
+      <button className="button is-primary">Save</button>
+    </form>
+  );
 }
 
 export default BookEdit;
--- books/src/components/BookShow.js
+++ books/src/components/BookShow.js
@@ -1,13 +1,33 @@
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+
 function BookShow({ book, onDelete }) {
-  const handleClick = () => {
+  const [showEdit, setShowEdit] = useState(false); // I don't want to showEdit by default
+
+  const handleDeleteClick = () => {
     onDelete(book.id);
   };
 
+  const handleEditClick = () => {
+    setShowEdit(!showEdit); // flips to the opposite it currently is
+  };
+
+  // Depending on the Value of showEdit, it will assign a Variable either "book.title" or an instance of the bookEdit component
+  let content = <h3>{book.title}</h3>;
+  // If showEdit is true
+  if (showEdit) {
+    content = <BookEdit book={book} />; // Display BookEdit component with "book" Prop, intead of "book.title"
+  }
+
   return (
-    <div className="book-show ">
-      {book.title}
+    <div className="book-show">
+      <div>{content}</div>
       <div className="actions">
-        <button className="delete" onClick={handleClick}>
+        {/* This is for showing "pencil" icon */}
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
           Delete
         </button>
       </div>
