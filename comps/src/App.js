6b932327ecae36b7794fcd125539b4a32da2bae9
Maksim Pasnitsenko <emexpas@outlook.com>
2023-06-25 15:10:38 +0300

    displays different text on buttons

---
+++ comps/src/App.js
@@ -0,0 +1,25 @@
+import Button from './Button';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button>Button 1</Button>
+      </div>
+      <div>
+        <Button>Button 2</Button>
+      </div>
+      <div>
+        <Button>Button 3</Button>
+      </div>
+      <div>
+        <Button>Button 4</Button>
+      </div>
+      <div>
+        <Button>Button 5</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ comps/src/Button.js
@@ -0,0 +1,6 @@
+function Button({ children }) {
+  // Underlying/Wrapped Element -> <button>Button</button>
+  return <button>{children}</button>;
+}
+
+export default Button;
