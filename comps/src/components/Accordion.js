c96e38ff6aa16c26472658d31d0ddcb4cf103b08
Maksim Pasnitsenko <emexpas@outlook.com>
2023-06-29 14:12:35 +0300

    conditional rendering in accordion comp

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
+
 function Accordion({ items }) {
-  const renderedItems = items.map(item => {
+  const [expandedIndex, setExpandedIndex] = useState(0); // the item at index 0 will be expanded by default, when the Component is first rendered
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    // if 'isExpanded' is truthy, then I will get back the last truthy value back, which will be the <div></div>
+    // if 'isExpanded' is false, then I will get back the first falsey value, which is going to be 'isExpanded', it's going to be false (and not rendered to the screen)
+
     return (
       <div key={item.id}>
         <div>{item.label}</div>
-        <div>{item.content}</div>
+        {isExpanded && <div>{item.content}</div>}
       </div>
     );
   });
