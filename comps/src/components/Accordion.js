aa0dc739ceed30a2760bf005d437e24251dff075
Maksim Pasnitsenko <emexpas@outlook.com>
2023-06-30 14:07:08 +0300

    added icons for accordion comp

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go';
 
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0); // the item at index 0 will be expanded by default, when the Component is first rendered
@@ -13,10 +14,18 @@ function Accordion({ items }) {
     // if 'isExpanded' is truthy, then I will get back the last truthy value back, which will be the <div></div>
     // if 'isExpanded' is false, then I will get back the first falsey value, which is going to be 'isExpanded', it's going to be false (and not rendered to the screen)
 
+    // it will show different text, depending upon the Value is expanded
+    const icon = (
+      <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+    );
+
     return (
       <div key={item.id}>
         {/* whenever the user clicks on one of the 'indexes', the Arrow Function will run and will invoke setExpanded with that particular item (only one Element will be expanded at a time*/}
-        <div onClick={() => handleClick(index)}>{item.label}</div>
+        <div onClick={() => handleClick(index)}>
+          {icon}
+          {item.label}
+        </div>
         {isExpanded && <div>{item.content}</div>}
       </div>
     );
