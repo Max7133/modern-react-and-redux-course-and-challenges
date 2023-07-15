55f83d526ca067fbabfe9806d33d5b465a325903
Maksim Pasnitsenko <emexpas@outlook.com>
2023-07-15 13:04:12 +0300

    useNavigation custom hook

--- comps/src/components/Link.js
+++ comps/src/components/Link.js
@@ -1,13 +1,12 @@
 import classNames from 'classnames';
-import { useContext } from 'react';
-import NavigationContext from '../context/navigation';
+import useNavigation from '../hooks/use-navigation';
 
 // to - the path to go to
 // children - some text that I want to show inside of the Anchor Element
 
 // the goal of Link Comp, is to make sure that clicking on an Anchor Element will not cause the page to refresh
 function Link({ to, children }) {
-  const { navigate } = useContext(NavigationContext); // gives back entire Object that is shared, I only take 'navigate' from it
+  const { navigate } = useNavigation(); // gives back entire Object that is shared, I only take 'navigate' from it
 
   const classes = classNames('text-blue-500');
 
--- comps/src/components/Route.js
+++ comps/src/components/Route.js
@@ -1,9 +1,8 @@
-import { useContext } from 'react';
-import NavigationContext from '../context/navigation';
+import useNavigation from '../hooks/use-navigation';
 
 function Route({ path, children }) {
   // reaching out to Context, for CurrentPath piece of state
-  const { currentPath } = useContext(NavigationContext);
+  const { currentPath } = useNavigation();
 
   if (path === currentPath) {
     // then show whatever was provided as the 'children' prop
