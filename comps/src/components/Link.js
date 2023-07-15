2ddedf9d66e9f8a22d0496e5c980ef43c4612ef5
Maksim Pasnitsenko <emexpas@outlook.com>
2023-07-15 12:59:26 +0300

    styled link component

--- comps/src/components/Link.js
+++ comps/src/components/Link.js
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { useContext } from 'react';
 import NavigationContext from '../context/navigation';
 
@@ -8,13 +9,23 @@ import NavigationContext from '../context/navigation';
 function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext); // gives back entire Object that is shared, I only take 'navigate' from it
 
+  const classes = classNames('text-blue-500');
+
   const handleClick = e => {
+    // if holding down
+    if (e.metaKey || e.ctrlKey) {
+      return;
+    }
     e.preventDefault();
 
     // Programatically navigating to some other path
     navigate(to);
   };
-  return <a onClick={handleClick}>{children}</a>;
+  return (
+    <a className={classes} href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;
