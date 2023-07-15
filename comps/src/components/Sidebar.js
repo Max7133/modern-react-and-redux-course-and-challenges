cd8930c3a14c575f5e36becf43c06c06dd9b4cd6
Maksim Pasnitsenko <emexpas@outlook.com>
2023-07-15 13:25:32 +0300

    added Sidebar Component

---
+++ comps/src/components/Sidebar.js
@@ -0,0 +1,27 @@
+import Link from './Link';
+
+function Sidebar() {
+  // Array of Objects
+  const links = [
+    { label: 'Dropdown', path: '/' },
+    { label: 'Accordion', path: '/accordion' },
+    { label: 'Buttons', path: '/buttons' },
+  ];
+
+  // for every Object I receive, it will receive a link
+  const renderedLinks = links.map(link => {
+    return (
+      <Link key={link.label} to={link.path}>
+        {link.label}
+      </Link>
+    );
+  });
+
+  return (
+    <div className="sticky top-0 overflow-y-scroll flex flex-col">
+      {renderedLinks}
+    </div>
+  );
+}
+
+export default Sidebar;
--- comps/src/pages/ButtonPage.js
+++ comps/src/pages/ButtonPage.js
@@ -2,9 +2,8 @@ import { GoBell, GoDatabase, GoClock } from 'react-icons/go';
 import Button from '../components/Button';
 
 function ButtonPage() {
-  const handleClick = () => {
-    console.log('click');
-  };
+  const handleClick = () => {};
+
   return (
     <div>
       <div>
