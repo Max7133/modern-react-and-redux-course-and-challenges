bcbf6b7264f579cbe65404df84fc11f41d99e422
Maksim Pasnitsenko <emexpas@outlook.com>
2023-07-17 12:27:25 +0300

    fixing the Modal window with 'createPortal()'

--- comps/src/components/Modal.js
+++ comps/src/components/Modal.js
@@ -1,9 +1,14 @@
+import ReactDOM from 'react-dom';
+
 function Modal() {
-  return (
+  return ReactDOM.createPortal(
+    // 1st Argument - all the JSX I want to show
     <div>
       <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
       <div className="absolute inset-40 p-10 bg-white">I'm a modal!</div>
-    </div>
+    </div>,
+    // 2nd Argument - reference to the DIV I added below before the closing Body Element in index.html </body>
+    document.querySelector('.modal-container')
   );
 }
 
