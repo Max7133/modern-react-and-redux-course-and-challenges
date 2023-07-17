1c7dab80306f0a070404613c5c501f8310ccbb9b
Maksim Pasnitsenko <emexpas@outlook.com>
2023-07-17 13:07:24 +0300

    the Modal is displayed wherecer I scrolled inside of Document

--- comps/src/components/Modal.js
+++ comps/src/components/Modal.js
@@ -1,14 +1,24 @@
 import ReactDOM from 'react-dom';
+import { useEffect } from 'react';
 
 function Modal({ onClose, children, actionBar }) {
+  useEffect(() => {
+    // this won't let me scroll down
+    document.body.classList.add('overflow-hidden');
+    // clean up func, which will be called after the Modal is removed
+    return () => {
+      document.body.classList.remove('overflow-hidden');
+    };
+  }, []);
+
   return ReactDOM.createPortal(
     // 1st Argument - all the JSX I want to show
     <div>
       <div
         onClick={onClose}
-        className="absolute inset-0 bg-gray-300 opacity-80"
+        className="fixed inset-0 bg-gray-300 opacity-80"
       ></div>
-      <div className="absolute inset-40 p-10 bg-white">
+      <div className="fixed inset-40 p-10 bg-white">
         <div className="flex flex-col justify-between h-full">
           {children}
           <div className="flex justify-end">{actionBar}</div>
