3a73bdc6d9dfc0cb290529ce8f6af9e7bcf6d0a2
Maksim Pasnitsenko <emexpas@outlook.com>
2023-07-21 16:32:09 +0300

    list of <td> elements is now exactly equal to the number of 'config' Objects

--- comps/src/components/Table.js
+++ comps/src/components/Table.js
@@ -3,14 +3,20 @@ function Table({ data, config }) {
     return <th key={column.label}>{column.label}</th>;
   });
 
+  // Mapping over every Object inside of the 'data' Array, so I'm getting those 'fruit' Objects
   const renderedRows = data.map(fruit => {
+    // 'column' Object is going to be each of the 'config' Objects that are put together inside the 'config' Array
+    const renderedCells = config.map(column => {
+      // It's going to have a list of <td></td> Elements exactly === to the number of 'config' Objects inside of the Array
+      return (
+        <td className="p-2" key={column.label}>
+          {column.render(fruit)}
+        </td>
+      );
+    });
     return (
       <tr className="border-b" key={fruit.name}>
-        <td className="p-3">{fruit.name}</td>
-        <td className="p-3">
-          <div className={`p-3 m-2 ${fruit.color}`}></div>
-        </td>
-        <td className="p-3">{fruit.score}</td>
+        {renderedCells}
       </tr>
     );
   });
