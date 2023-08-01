500df6335480cb0ff2c53ead82bfdd4d62fd033c
Maksim Pasnitsenko <emexpas@outlook.com>
2023-08-01 11:24:47 +0300

    CounterPage with useState version

--- comps/src/pages/CounterPage.js
+++ comps/src/pages/CounterPage.js
@@ -4,6 +4,7 @@ import Panel from '../components/Panel';
 
 function CounterPage({ initialCount }) {
   const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
 
   const increment = () => {
     setCount(count + 1);
@@ -11,6 +12,18 @@ function CounterPage({ initialCount }) {
 
   const decrement = () => {
     setCount(count - 1);
+  };
+  const handleChange = e => {
+    // || 0 - if I get back NaN, that is a falsy value, and so instead of a NaN, it will assign 0 to the value variable
+    const value = parseInt(e.target.value) || 0; // parseInt - it will show as number
+    setValueToAdd(value);
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    // taking the valueToAdd piece of State, and update the 'count' piece of State with it
+    setCount(count + valueToAdd);
+    // and then reset valueToAdd to 0
+    setValueToAdd(0);
   };
 
   return (
@@ -21,9 +34,12 @@ function CounterPage({ initialCount }) {
         <Button onClick={decrement}>Decrement</Button>
       </div>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
         <input
+          // || "" - so that the input won't always show 0 by default
+          value={valueToAdd || ''}
+          onChange={handleChange}
           type="number"
           className="p-1 m-3 bg-gray-50 border border-gray-300"
         />
