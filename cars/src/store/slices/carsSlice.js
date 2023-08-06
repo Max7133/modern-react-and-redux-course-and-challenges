773e0a5e3e8d9e9d8c14975a1d835d4049b812de
Maksim Pasnitsenko <emexpas@outlook.com>
2023-08-06 12:33:26 +0300

    carsSlice starter code

---
+++ cars/src/store/slices/carsSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, nanoid } from '@redux/toolkit';
+
+const carsSlice = createSlice({
+  name: 'cars',
+  initialState: {
+    searchTerm: '',
+    cars: [], // will grow over time
+  },
+  reducers: {
+    changeSearchTerm(state, action) {
+      state.searchTerm = action.payload;
+    },
+    addCar(state, action) {
+      // Assumption:
+      // action.payload === { name: 'ab', cost: 140 }
+      // I NEED to make sure when the 'action' is dispatched, I provide a Payload that looks like the one I wrote above with 'name' & 'cost'
+      // THE REASON is if I won't then this Reducer won't work correctly
+      // OTHER REASON this 'carsSlice' CANNOT communicate with 'formSlice'
+      state.cars.push({
+        name: action.payload.name,
+        cost: action.payload.cost,
+        id: nanoid(), // random id from redux toolkit
+      });
+    },
+    removeCar(state, action) {
+      // Assumption:
+      // action.payload === the ID of the car I want to remove
+      // I NEED to make sure when the 'action' Object is dispatched, that says to removeCar, I need to be sure that the Payload Property I include the ID of the car I want to remove
+      const updated = state.cars.filter(car => {
+        return car.id !== action.payload;
+      });
+      state.cars = updated;
+    },
+  },
+});
+
+export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
+export const carsReducer = carsSlice.reducer;
