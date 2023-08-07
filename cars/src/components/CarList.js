076002aff8540a4d4df94f61a03834044784d58b
Maksim Pasnitsenko <emexpas@outlook.com>
2023-08-07 16:29:44 +0300

    checking if I can submit a car, removed double key names

--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -1,4 +1,13 @@
+import { useSelector } from 'react-redux';
+
 function CarList() {
+  // getting access to the list of cars from Redux Store and print them out
+  const cars = useSelector(state => {
+    return state.cars.carsData;
+  });
+
+  console.log(cars);
+
   return <div>CarList</div>;
 }
 
--- cars/src/store/slices/carsSlice.js
+++ cars/src/store/slices/carsSlice.js
@@ -4,7 +4,7 @@ const carsSlice = createSlice({
   name: 'cars',
   initialState: {
     searchTerm: '',
-    cars: [], // will grow over time
+    carsData: [], // will grow over time
   },
   reducers: {
     changeSearchTerm(state, action) {
@@ -16,7 +16,7 @@ const carsSlice = createSlice({
       // I NEED to make sure when the 'action' is dispatched, I provide a Payload that looks like the one I wrote above with 'name' & 'cost'
       // THE REASON is if I won't then this Reducer won't work correctly
       // OTHER REASON this 'carsSlice' CANNOT communicate with 'formSlice'
-      state.cars.push({
+      state.carsData.push({
         name: action.payload.name,
         cost: action.payload.cost,
         id: nanoid(), // random id from redux toolkit
@@ -26,10 +26,10 @@ const carsSlice = createSlice({
       // Assumption:
       // action.payload === the ID of the car I want to remove
       // I NEED to make sure when the 'action' Object is dispatched, that says to removeCar, I need to be sure that the Payload Property I include the ID of the car I want to remove
-      const updated = state.cars.filter(car => {
+      const updated = state.carsData.filter(car => {
         return car.id !== action.payload;
       });
-      state.cars = updated;
+      state.carsData = updated;
     },
   },
 });
