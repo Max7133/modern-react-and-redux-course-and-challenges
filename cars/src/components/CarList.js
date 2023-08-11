5050dfefdf63904f391a15ad6ecd12b7dc380479
Maksim Pasnitsenko <emexpas@outlook.com>
2023-08-11 10:22:21 +0300

    mapping over car list

--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -6,9 +6,31 @@ function CarList() {
     return state.cars.carsData;
   });
 
-  console.log(cars);
+  const handleCarDelete = car => {};
 
-  return <div>CarList</div>;
+  // for every car, return a div element
+  const renderedCars = cars.map(car => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)} // the 'car' I'm iterating over
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
