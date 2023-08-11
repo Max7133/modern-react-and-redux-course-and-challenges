e5e30242d95e0eefac746d4f26e47f7dee6228ca
Maksim Pasnitsenko <emexpas@outlook.com>
2023-08-11 12:37:27 +0300

    total cost with filtered cars and logic for it

--- cars/src/components/CarValue.js
+++ cars/src/components/CarValue.js
@@ -1,5 +1,17 @@
+import { useSelector } from 'react-redux';
+
 function CarValue() {
-  return <div>CarValue</div>;
+  const totalCost = useSelector(({ cars: { carsData, searchTerm } }) =>
+    carsData
+      .filter(car =>
+        // for every 'car' I want t return 'true' if its lowerCase name includes the lowerCase of searchTerm
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      // then out of the filtered list of 'cars', I want to take all these different 'cars' costs and sum them all up into 1 single number
+      .reduce((acc, car) => acc + car.cost, 0)
+  );
+
+  return <div className="car-value">Total Cost: ${totalCost}</div>;
 }
 
 export default CarValue;
