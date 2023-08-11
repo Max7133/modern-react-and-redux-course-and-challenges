cd165636a37bf0d43c12340ba2d7395f3a526d78
Maksim Pasnitsenko <emexpas@outlook.com>
2023-08-11 13:05:16 +0300

    shows bold car name, when there is duplicate names or letters (logic) (app DONE)

--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -9,15 +9,23 @@ function CarList() {
   // getting access to the list of cars from Redux Store and print them out
   // in addition, Derived State (state that can be calculated from using the existing state) is best to put logic in useSelector()
   // using destructuring so I can use only the State that I care about
-  // FILTERED LIST
-  const cars = useSelector(({ cars: { carsData, searchTerm } }) => {
-    //return state.cars.carsData;
-    // return 'true' for keeping the car and return it inside the new Array
-    // if the 'car' has a 'name', that includes the searchTerm,
-    return carsData.filter(car =>
-      car.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-  });
+  // 'cars' and 'name' - FILTERED LIST CARS and NAME INSIDE OF AN OBJECT
+  const { cars, name } = useSelector(
+    ({ form, cars: { carsData, searchTerm } }) => {
+      //return state.cars.carsData;
+      // return 'true' for keeping the car and return it inside the new Array
+      // if the 'car' has a 'name', that includes the searchTerm,
+      const filteredCars = carsData.filter(car =>
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+
+      // useSelector is going to return
+      return {
+        cars: filteredCars, // list of cars
+        name: form.name, // name property
+      };
+    }
+  );
 
   // updating the State means dispatch the Action Object
   const handleCarDelete = car => {
@@ -26,8 +34,13 @@ function CarList() {
 
   // for every car, return a div element
   const renderedCars = cars.map(car => {
+    // DECIDE IF THIS CAR NAME SHOULD BE BOLD (for e.g. when there are duplicate 'name' of cars or same letters)
+    // state.form.name - access to the value as the 'name' variable
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
     return (
-      <div key={car.id} className="panel">
+      // applying bold if bold is true
+      <div key={car.id} className={`panel ${bold && 'bold'}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
