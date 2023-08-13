bc80a2f61c4d8672d50391e08ffa162c88d680d9
Maksim Pasnitsenko <emexpas@outlook.com>
2023-08-13 17:05:14 +0300

    using loading state

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,16 +1,30 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../store';
 
 function UsersList() {
   const dispatch = useDispatch();
+  // useSelector - Accessing State inside of a Component
+  // state - Big State Object
+  const { isLoading, data, error } = useSelector(state => {
+    return state.users; // 'users' portion of State { data: [], isLoading false, error: null }
+  });
 
   // will run automatically when the 1st time the Coomponent is rendered onto the screen
   useEffect(() => {
     // Running Thunk
     dispatch(fetchUsers());
   }, [dispatch]); // usually just [] is fine, used [dispatch] so the ESLint error will go away
-  return 'Users List';
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching data...</div>;
+  }
+
+  return <div>{data.length}</div>; // how many Users exist inside of the 'data' Array
 }
 
 export default UsersList;
