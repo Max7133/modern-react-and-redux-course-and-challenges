55ece052e489b5fd3a3b1e778fbb3df6a3d116d2
Maksim Pasnitsenko <emexpas@outlook.com>
2023-08-15 17:01:21 +0300

    Option 1 move all loading state to components (part1)

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,21 +1,36 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers, addUser } from '../store';
 import Button from './Button';
 import Skeleton from './Skeleton';
 
 function UsersList() {
+  // whether or not it's loading the big list of users (for showing and not showing the skeleton loader Comp)
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState(null);
   const dispatch = useDispatch();
   // useSelector - Accessing State inside of a Component
   // state - Big State Object
-  const { isLoading, data, error } = useSelector(state => {
+  const { data } = useSelector(state => {
     return state.users; // 'users' portion of State { data: [], isLoading false, error: null }
   });
 
-  // will run automatically when the 1st time the Coomponent is rendered onto the screen
+  // will run automatically when the 1st time the Component is rendered onto the screen
   useEffect(() => {
+    setIsLoadingUsers(true); // updates State
     // Running Thunk
-    dispatch(fetchUsers());
+    // starts the request
+    // unwrap() - is going to give a brand new Promise back, and the Promise it gives back is going to follow the CONVENTIONAL RULES
+    // dispatch RULES - .then() is called when the request is FULFILLED or REJECTED
+    // CONVENTIONAL RULES - .then() ONLY if the request is FULFILLED and .catch() ONLY if the request REJECTED
+    dispatch(fetchUsers())
+      .unwrap()
+      //.then(() => setIsLoadingUsers(false)) // I DON'T NEED IT BECAUSE OF finally(), it will run no mater what at the end
+      .catch(
+        err => setLoadingUsersError(err)
+        //setIsLoadingUsers(false);
+      )
+      .finally(() => setIsLoadingUsers(false));
   }, [dispatch]); // usually just [] is fine, used [dispatch] so the ESLint error will go away
 
   const handleUserAdd = () => {
@@ -23,13 +38,14 @@ function UsersList() {
     dispatch(addUser()); // adds ran gen user to the user list (data: []) from 'usersSlice'
   };
 
-  if (isLoading) {
+  if (isLoadingUsers) {
     // h-10 - a Height of 10 rem
     // w-full - expand in the X direction as much as possible
     return <Skeleton times={6} className="h-10 w-full" />; // show 6 gray boxes
   }
 
-  if (error) {
+  // if is not null
+  if (loadingUsersError) {
     return <div>Error fetching data...</div>;
   }
 
