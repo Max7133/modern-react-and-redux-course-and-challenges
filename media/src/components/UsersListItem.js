0e49ff6e1e33247b5b7fca998485bd07bce2233c
Maksim Pasnitsenko <emexpas@outlook.com>
2023-08-16 18:55:29 +0300

    deletes the user (part 1/2) still needs the FIX, + reason

--- media/src/components/UsersListItem.js
+++ media/src/components/UsersListItem.js
@@ -1,8 +1,24 @@
+import { GoTrash } from 'react-icons/go';
+import Button from './Button';
+import { removeUser } from '../store';
+import { useThunk } from '../hooks/use-thunk';
+
 // the Arg receives a Prop of the 'user' Object from UsersList comp
 function UsersListItem({ user }) {
+  const [doRemoveUser, isLoading, error] = useThunk(removeUser);
+
+  const handleUserRemove = () => {
+    doRemoveUser(user);
+  };
+
   return (
     <div className="mb-2 border rounded">
       <div className="flex p-2 justify-between items-center cursor-pointer">
+        {/* loading={isLoading} - shows spinner */}
+        <Button loading={isLoading} onClick={handleUserRemove}>
+          <GoTrash />
+        </Button>
+        {error && <div>Error deleting user.</div>}
         {user.name}
       </div>
     </div>
--- media/src/store/thunks/removeUser.js
+++ media/src/store/thunks/removeUser.js
@@ -5,7 +5,12 @@ import axios from 'axios';
 const removeUser = createAsyncThunk('users/remove', async user => {
   const response = await axios.delete(`http://localhost:3005/users/${user.id}`);
 
-  return response.data; // FIX
+  // FIX
+  // THE REASON:
+  // whetever I return show up as 'action.payload' inside of the Reducer
+  // when I send the DELETE request off to the JSON server, ther respone is going to be an Empty Object
+  // so when returning 'response.data', I'm returning an EMPTY OBJECT, therefore I don't have any info on who supposed to be deleted!
+  return response.data;
 });
 
 export { removeUser };
