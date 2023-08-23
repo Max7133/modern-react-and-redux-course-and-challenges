30111d3bb47675b595f030400e13980e3d13899c
Maksim Pasnitsenko <emexpas@outlook.com>
2023-08-23 13:55:47 +0300

    showing and rendering photos (no styling yet)

--- media/src/components/PhotosList.js
+++ media/src/components/PhotosList.js
@@ -1,10 +1,14 @@
 // Whenever PhotosList shows on the screen, it will make a request and get all the different Photos for this particular 'album' PhotosList({ album })
 import { useAddPhotoMutation, useFetchPhotosQuery } from '../store';
 import Button from './Button';
+import Skeleton from './Skeleton';
+import PhotosListItem from './PhotosListItem';
 
 // Whenever I show PhotosList, I'm expecting to receive an 'album' as a Prop
 function PhotosList({ album }) {
-  useFetchPhotosQuery(album); // from ASSUMPTIONS, when I call the 'query' or those 2 Mutations, I have to provide an Arg of 'album' (it will take it and sent of to 'fetchPhotos' query function Endpoint)
+  // whenever I call 'useFetchPhotosQuery' Hook, it gets back the 'results Object'
+  // destructuring 'results Object'
+  const { data, isFetching, error } = useFetchPhotosQuery(album); // from ASSUMPTIONS, when I call the 'query' or those 2 Mutations, I have to provide an Arg of 'album' (it will take it and sent of to 'fetchPhotos' query function Endpoint)
   // running 'mutation Hook' that's going to give me back an Array with a couple of Elements
   // I'm intrested in these (I called the 'results' Object - addPhotoResults)
   const [addPhoto, addPhotoResults] = useAddPhotoMutation();
@@ -12,6 +16,19 @@ function PhotosList({ album }) {
   const handleAddPhoto = () => {
     addPhoto(album); // whenever it runs this ENDPOINT, I need to provide the 'album' because my ENDPOINT is entirely configured to expect to receive an 'album' (it's going to use that 'album' to configure the Request)
   };
+
+  let content;
+  if (isFetching) {
+    content = <Skeleton className="h-8 w-8" times={4} />;
+  } else if (error) {
+    content = <div>Error fetching photos...</div>;
+  } else {
+    // 'data' - Array of 'photos', and for each individual photo it will show a 'photosListItem'
+    content = data.map(photo => {
+      // because it builds a list, I need to provide a 'key' Prop and the 'photo' itself as a Prop, so that the Component itself show some info about the 'photo'
+      return <PhotosListItem key={photo.id} photo={photo} />;
+    });
+  }
 
   return (
     <div>
@@ -22,6 +39,7 @@ function PhotosList({ album }) {
           + Add Photo
         </Button>
       </div>
+      <div>{content}</div>
     </div>
   );
 }
--- media/src/components/PhotosListItem.js
+++ media/src/components/PhotosListItem.js
@@ -1,5 +1,10 @@
-function PhotosListItem() {
-  return 'PhotosListItem';
+function PhotosListItem({ photo }) {
+  // 'photo' Prop itself is going to have an ID, URL, album.id (only need URL for showing and rendering the 'photo')
+  return (
+    <div>
+      <img className="h-20 w-20" src={photo.url} alt="random pic" />
+    </div>
+  );
 }
 
 export default PhotosListItem;
