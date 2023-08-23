dc4424334e83922ec115326b42aa1a548dd41de5
Maksim Pasnitsenko <emexpas@outlook.com>
2023-08-23 12:55:28 +0300

    small fix useFetchPhotosQuery NOT useFetchAlbumsQuery

--- media/src/components/PhotosList.js
+++ media/src/components/PhotosList.js
@@ -1,8 +1,8 @@
 // Whenever PhotosList shows on the screen, it will make a request and get all the different Photos for this particular 'album' PhotosList({ album })
-import { useFetchAlbumsQuery } from '../store';
+import { useFetchPhotosQuery } from '../store';
 // Whenever I show PhotosList, I'm expecting to receive an 'album' as a Prop
 function PhotosList({ album }) {
-  useFetchAlbumsQuery(album); // from ASSUMPTIONS, when I call the 'query' or those 2 Mutations, I have to provide an Arg of 'album' (it will take it and sent of to 'fetchPhotos' query function Endpoint)
+  useFetchPhotosQuery(album); // from ASSUMPTIONS, when I call the 'query' or those 2 Mutations, I have to provide an Arg of 'album' (it will take it and sent of to 'fetchPhotos' query function Endpoint)
 
   return 'PhotosList';
 }
